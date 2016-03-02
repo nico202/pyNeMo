59ac06529cbfe5e2ccb6f31a16e9b22b8c08170e
Nicolò Balzarotti <anothersms@gmail.com>
2016-03-02 11:49:05 +0100

    config: better descripion

--- config.py
+++ config.py
@@ -17,4 +17,5 @@ CUDA_BACKEND = 0
 BATCH_CONFIRM_SHOW_IMAGE = True
 #Asks you if you are sure you don't want to save images
 BATCH_CONFIRM_NO_SAVE_IMAGE = True
+#How frequently we should save batch step
 BATCH_SAVE_EVERY = 10
