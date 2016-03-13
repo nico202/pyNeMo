75554b1e76f6e551be03a5f51f7b91a933525bc3
Nicolò Balzarotti <anothersms@gmail.com>
2016-03-13 12:00:03 +0100

    Nix: removed unused dependencies

--- default.nix
+++ default.nix
@@ -2,28 +2,14 @@ with import <nixpkgs> {};
     with pkgs.python27Packages;
     
     buildPythonPackage { 
-      name = "impurePythonEnv";
+      name = "pyNeMo";
       buildInputs = [
          git
-         libxml2
-         libxslt
-         libzip
          python27Packages.virtualenv
          python27Packages.pandas
          python27Packages.numpy
          python27Full
-         stdenv
-         zlib ];
+         ];
       src = null;
-      # When used as `nix-shell --pure`
-      shellHook = ''
-      unset http_proxy
-      export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
-      '';
-      # used when building environments
-      extraCmds = ''
-      unset http_proxy # otherwise downloads will fail ("nodtd.invalid")
-      export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
-      '';
     }
 
