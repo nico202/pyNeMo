a1b9c197e6033a4cbe6c99ae92caf43b0ba49620
= <anothersms@gmail.com>
2016-03-14 05:46:17 +0100

    nix: added PIL (images still not working)

--- default.nix
+++ default.nix
@@ -5,8 +5,9 @@ with import <nixpkgs> {};
       name = "pyNeMo";
       buildInputs = [
          git
-         python27Packages.virtualenv
          python27Packages.pandas
+         python27Packages.virtualenv
+         python27Packages.pillow
          python27Packages.numpy
          python27Full
          ];
