e514288b067ba5d3521dca9ee6ce76f67a9b8e19
= <anothersms@gmail.com>
2016-03-19 15:01:23 +0100

    Nix: added modules for multistation

--- default.nix
+++ default.nix
@@ -11,6 +11,8 @@ with import <nixpkgs> {};
          python27Packages.numpy
          python27Packages.pip #Not required, just for testing
          python27Packages.matplotlib
+         python27Packages.web #Required for multiple-machine system
+         python27Packages.requests #Required for multiple-machine system
          python27Full
 	 yarp
          #Enable image show
