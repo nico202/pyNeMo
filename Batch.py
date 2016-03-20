aeb4c41d6e040c095fffd1da8f70f37211bc4bb3
Nicolò Balzarotti <anothersms@gmail.com>
2016-03-20 03:47:05 +0100

    Maybe native batch works (still not tested)

--- Batch.py
+++ Batch.py
@@ -7,12 +7,16 @@ import os
 import re
 import string
 
+from libs.runParser import parse_args
+
 from libs.IO import is_folder, hashDict, write_batch_log, saveKey
 from libs.IO import cprint
 from plugins.importer import import_history
 import config
 
 from sys import argv, exit
+
+from libs.simulations import main_simulation_run
 
 print ("\n\n\n\n\n\n\n----------------")
 
@@ -67,7 +71,6 @@ l = open("batch_history.log", 'a')
 l.write("%s, %s" % (output_dir, args))
 l.close()
 
-
 ranges = True
 def missing(input_string):
     #TODO: check [ number + , + number + , + number ]
@@ -109,7 +112,7 @@ session_hash = str(hashDict(real_commands))
 #Save commands list
 if_folder("./commands")
 commands_file = "./commands/" + session_hash + "_commands"
-print ("Saving %s commands to file: %s" % (len(real_commands), commands_file))
+cprint ("Saving %s commands to file: %s" % (len(real_commands), commands_file), 'info')
 saveKey(commands_file, commands)
 cprint("Running in 5s", "warning")
 time.sleep(5)
@@ -117,7 +120,7 @@ time.sleep(5)
 #Start
 is_folder (output_dir)
 
-print("We'll use %s as output dir" % (output_dir))
+cprint("We'll use %s as output dir" % (output_dir), 'info')
 
 #Let's run the simulations (surely not the best way, but does the job)
 #TODO: add the loop inside Runner?
@@ -151,8 +154,97 @@ for com in real_commands:
             time.sleep(5)
             forced_quit = False
             next_sleep = False
+        #Start. Take time
+        start_time = time.time()
         print ("Lap %s / %s" % (lap , laps))
-        subprocess.call("%s Runner.py %s" % (python, com), shell = True)
+        ##Replace subprocess: call it directly to save time
+        parser = parse_args()
+        print(com) #Debug
+        args = parser.parse_args(com.split()) #That way is 100% compatible with the old os call
+        exit("Exiting, debug") #Debug
+        
+        #Load all the parameters (choose if/when read from CLI/config.py)
+        use_config = True if os.path.isfile("config.py") else False
+        if use_config: import config
+        
+        network_file = args.network_file
+
+        config_steps = config.STEPS if use_config else 0
+        steps = time_to_steps(args.steps) if args.steps != None else config_steps
+        
+        config_history = config.HISTORY_DIR if use_config else 0
+        output_dir = args.history_dir or config_history
+        
+        config_cuda = config.TRY_CUDA if use_config else 0
+        use_cuda = args.use_cuda if args.use_cuda != None else config_cuda
+        
+        config_cuda_backend = config.CUDA_BACKEND if use_config else 0
+        cuda_backend = args.cuda_backend or config_cuda_backend
+        try:
+            cuda_backend = int(cuda_backend)
+        except ValueError:
+            exit("Error, wrong cuda_backend format. Must be a int")
+            
+        vue_prehook = args.vue_prehook
+        vue_posthook = args.vue_posthook
+            
+        #Robot args
+        control_robot = args.control_robot
+        robot_mode = args.robot_mode #FIXME
+        
+        disable_sensory = args.disable_sensory
+        
+        ########################################
+
+        #L1
+        networks, config_file_name =\
+                import_network (
+                    (network_file, (vue_prehook, vue_posthook))
+                    , (use_cuda, cuda_backend)
+                    , (disable_sensory))
+
+        #L1
+        nemo_simulation = networks[0]
+        to_save = networks[1][0]
+        neuron_number = networks[1][1]
+        stimuli_dict = networks[1][2]
+        
+        #L2
+        if (sensory_neurons_in or sensory_neurons_out):
+            from libs.pYARP import RobotYARP #Import only if strictly needed
+            robot = RobotYARP ()
+        else:
+            robot = None
+
+        #L3
+        if not disable_sensory: #CLI
+            sensory_neurons_in, sensory_neurons_out  = networks[1][3]
+        else:
+            sensory_neurons_in = []
+            sensory_neurons_out = []
+
+        output = main_simulation_run(
+            {
+                "steps": steps
+                , "use_cuda": use_cuda
+                , "cuda_backend": cuda_backend
+            }#general_config
+            , (nemo_simulation, (to_save, neuron_number), stimuli_dict) #L1
+            , (robot) #L2
+            , (sensory_neurons_in, sensory_neurons_out) #L3
+        )
+
+        general_config_out = {"steps": output["ran_steps"]} #Add various robot params
+        #Save/process output + input
+        uniqueId = hashDict(general_config_out)\
+                   + "_" + hashDict(dict_config)
+        
+        #Step is included in the output
+        saveKey(uniqueId + "_output", output, output_dir)
+        
+        #Save the log
+        write_log(uniqueId, output_dir = output_dir)
+        
         lap +=1
         #save only every X laps and on keyboard interrupt
         if not lap % config.BATCH_SAVE_EVERY:
@@ -163,6 +255,10 @@ for com in real_commands:
             cprint("This round mean step time: %s" % (time_diff / config.BATCH_SAVE_EVERY))
             write_batch_log(session_hash + "_batch", lap, output_dir)
             cprint("-------------------")
+        else:
+            end_time = time.time()
+            cprint ("Realtime ratio: %sX" % (output["ran_steps"]/((start_time - end_time)*1000)), 'info')
+        
     except KeyboardInterrupt:
         if not forced_quit:
             write_batch_log(session_hash + "_batch", lap, output_dir)
