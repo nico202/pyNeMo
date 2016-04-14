1f9e6acceeb8af8398f9f775857e49f7eb304de4
= <anothersms@gmail.com>
2016-04-14 16:21:12 +0200

    Added NIX python yarp support (finally!)

---
+++ default-yarp.nix
@@ -0,0 +1,26 @@
+with import <nixpkgs> {};
+    with pkgs.python27Packages;
+    
+    buildPythonPackage { 
+      name = "pyNeMo";
+      buildInputs = [
+         git
+         python27Packages.pandas
+         python27Packages.virtualenv
+         python27Packages.pillow
+         python27Packages.numpy
+         python27Packages.pip #Not required, just for testing
+         python27Packages.matplotlib
+         python27Packages.dill
+         python27Packages.web #Required for multiple-machine system
+         python27Packages.requests #Required for multiple-machine system
+         python27Packages.bpython
+         python27Full
+	 yarp
+         #Enable image show
+         imagemagick
+         ( pkgs.callPackage ./yarp-python { })
+         ];
+      src = null;
+    }
+
