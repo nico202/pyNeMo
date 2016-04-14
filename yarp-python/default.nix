71b8811ac13ef77a0b370a76a42b314b97f7c20c
= <anothersms@gmail.com>
2016-04-14 17:23:18 +0200

    nix: better package

--- yarp-python/default.nix
+++ yarp-python/default.nix
@@ -1,32 +1,23 @@
-{ stdenv, fetchFromGitHub, ace, cmake, python, swig, yarp
+{ stdenv, fetchFromGitHub, ace, cmake, python, ruby, swig, yarp
 }:
 
 stdenv.mkDerivation rec {
-  name = "yarp-python-${version}";
-  version = "2.3.64";
-  src = fetchFromGitHub {
-    owner = "robotology";
-    repo = "yarp";
-    rev = "v2.3.64";
-    sha256 = "0x9sdc8d6rppzf1kx53w0yjlnmz7h75qv62yd3ls09w3cy7nb5x7";
-  };
+  name = "yarp-bindings-${version}";
+  version = yarp.version;
 
-#  phases = [ "postInstall" ];
+  src = yarp.src;
 
   preConfigure = "cd bindings";
 
-  cmakeFlags = [ "-DCREATE_PYTHON=TRUE" ];
+  cmakeFlags = [
+    "-DCREATE_PYTHON=TRUE"
+    "-DCREATE_RUBY=TRUE"
+  ];
 
-  buildInputs = [ cmake ace swig yarp python ];
+  buildInputs = [ cmake ace ruby swig yarp python ];
 
   enableParallelBuilding = true;
 
-  meta = {
-    description = "Yet Another Robot Platform";
-    homepage = http://yarp.it;
-    license = stdenv.lib.licenses.lgpl21;
-    platforms = stdenv.lib.platforms.linux;
-    maintainers = [ stdenv.lib.maintainers.nico202 ];
-  };
+  meta = yarp.meta;
 }
 
