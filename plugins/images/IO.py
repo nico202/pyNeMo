1b183d1e5dea888c9b575c11a4fec48254c23c49
= <anothersms@gmail.com>
2016-04-16 16:53:05 +0200

    f

--- plugins/images/IO.py
+++ plugins/images/IO.py
@@ -43,7 +43,7 @@ def ImageFromSpikes(input_spikes, file_path = "./latest.png", show = True, save 
             i = [l for l in i if l < xmax and l > xmin]
             new_spikes.append(i)
         spikes = new_spikes
-        
+
     if save:
         cprint("\t*\tSaving image to %s" % file_path, 'info')
     sp.set_fig_name(file_path)
@@ -68,8 +68,8 @@ def ImageFromMembranes(all_membranes):
 def ImageFromMembrane(
         neuron_id
         , Vm_list
-        , save = False
-        , show = True
+        , save=False
+        , show=True
         , stimuli = [0, 0]
         , title = ""
 ):
@@ -94,3 +94,98 @@ def ImageFromMembrane(
         plt.title(title)
 
     return plt
+
+def ImageFromAngles(
+        (angles_input
+        , pyspike_output)
+        , save=True
+        , show=False
+        , title="Joints Angles"
+        , file_path='./latest.png'
+): #Similar to ImageFromMembrane?
+    import matplotlib.pyplot as plt
+    import numpy as np
+    angles_read = angles_input
+    angles_wrote = pyspike_output
+    if not len(angles_read):
+        print("No angles data!")
+        return False
+
+    
+    # for ms_couple in angles_wrote:
+    #     for i, data in enumerate(ms_couple):
+            
+#        print angles_wrote
+    #FIXME: non riesco ad assegnare a una lista O.o
+    jnt_number = len(angles_read[0])
+    angles_A = [] 
+    angles_B = []
+    i = 0
+    for coppia in angles_read:
+        angles_A.append(coppia[:][0])
+        angles_B.append(coppia[:][1])
+        i += 1
+    angles = [angles_A, angles_B]
+    plots = []
+    init = False
+    #FIXME: one plot above the other
+    # for values in angles:
+    #     x = len(values)
+    #     x = np.array(range(0, x))
+    #     if not init:
+    #         fig, ax1 = plt.subplots()
+    #         plots.append(ax1)
+    #         plots[0].plot(x, np.array(values))
+    #         plots[0].set_xlabel('time (ms)')
+    #         plots[0].set_ylabel('Angle Joint %s' % joint)
+    #     else:
+    #         plots.append(plots[0].twinx())
+    #         plots[-1].plot(values, colors = 'r')
+    #         plots[-1].set_xlabel('time\n(ms)')
+    #         plots[-1].set_ylabel('Angle Joint %s' % joint)
+    #     init = True
+
+    a = len(angles[0])
+    b = len(angles[1])
+    a = np.array(range(0, a))
+    b = np.array(range(0, b))
+
+    fig, ax1 = plt.subplots()
+    plots.append(ax1)
+    ax1.plot(a, np.array(angles[0]))
+    ax1.set_xlabel('time (ms)')
+    ax1.set_ylabel('Angle Joint %s' % (1))
+
+    ax2 = ax1.twinx()
+    ax2.plot(angles[1], color = 'r')
+    ax2.set_xlabel('time (ms)')
+    ax2.set_ylabel('Angle Joint %s' % (2))    
+
+    if pyspike_output:
+        out_zero = []
+        out_one = []
+        for ms in pyspike_output:
+            out_zero.append(ms[0][1])
+            out_zero = out_zero[:]
+            out_one.append(ms[1][1])
+            out_one = out_one[:]
+
+        ax3 = ax2.twinx()
+        ax3.plot(out_zero, color = 'g')
+        ax3.set_xlabel('time (ms)')
+        ax3.set_ylabel('pySpike %s' % (2))    
+
+        ax4 = ax3.twinx()
+        ax4.plot(out_one, color = 'c')
+        ax4.set_xlabel('time (ms)')
+        ax4.set_ylabel('pySpike %s' % (2))    
+
+    if title:
+        plt.title(title)
+        
+    if save:
+        cprint("\t*\tSaving image to %s" % file_path, 'info')
+        plt.savefig(file_path)
+        
+    return True
+    
