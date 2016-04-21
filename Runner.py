86521eee92b8ac61535aa5ca5f2646004632e3f1
= <anothersms@gmail.com>
2016-04-21 22:37:34 +0200

    Runner.py: pylint-ing (4.23)

--- Runner.py
+++ Runner.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python2
-
-#"Front-end" for the real script
-
-from sys import exit, stderr
+"""
+"Front-end" for the real script
+"""
 
 #Timing
 import time
@@ -22,14 +21,13 @@ from libs.IO import saveFile
 from libs.simulations import main_simulation_run
 from libs.pYARP import RobotYARP
 
-import config
 if __name__ == "__main__":
     #Check dependencyes:
     required_modules = [
         "nemo"
         , "scipy"
-#        , "yarp" #FIXME: not needed if no sensory
-#       , "pygazebo"
+        # , "yarp" #FIXME: not needed if no sensory
+        # , "pygazebo"
         , "shutil"
     ]
     dependency_check(required_modules)
@@ -41,7 +39,8 @@ if __name__ == "__main__":
 
     #Load all the parameters (choose if/when read from CLI/config.py)
     use_config = True if os.path.isfile("config.py") else False
-    if use_config: import config
+    if use_config:
+        import config
 
     network_file = args.network_file
 
@@ -72,17 +71,16 @@ if __name__ == "__main__":
 
     #Create general_config, the configuration that will be passed to our simulation
     general_config = {
-        "steps":steps
-        , "use_cuda": use_cuda
-        , "cuda_backend": cuda_backend
+        "steps":steps,
+        "use_cuda": use_cuda,
+        "cuda_backend": cuda_backend
     }
 
     #Import the network, tranform it to a valid nemo.Network object
     if ".py" in network_file and (vue_prehook != "#No prehooks"
                                   or vue_posthook != "#No posthooks"):
         exit("Refusing to run with hooks on py file (not supported yet")
-    
-    
+
     networks, config_file_name =\
         import_network(
             (network_file, (vue_prehook, vue_posthook))
@@ -99,31 +97,32 @@ if __name__ == "__main__":
     else:
         sensory_neurons_in = []
         sensory_neurons_out = []
-    
+
     neurons = networks[1][4]
     synapses = networks[1][5]
     network_name = networks[1][6]
-    
+
     #Save Config
     dict_config = {
-        "neurons":neurons
-        , "sensory_neurons":(sensory_neurons_in, sensory_neurons_out)
-        , "save": to_save
-        , "step_input": stimuli_dict
-        , "synapses": synapses
-        , "name": network_name
+        "neurons": neurons,
+        "sensory_neurons": (sensory_neurons_in, sensory_neurons_out),
+        "save": to_save,
+        "step_input": stimuli_dict,
+        "synapses": synapses,
+        "name": network_name
     }
+
     config_dict_hash = hashDict(dict_config)
     saveKey(config_dict_hash + "_input"
             , dict_config
             , output_dir)
-    
+
     saveFile(
         config_file_name
         , output_dir + "/" + config_dict_hash + "_input.py")
 
     #Define robot
-    if (sensory_neurons_in or sensory_neurons_out):
+    if sensory_neurons_in or sensory_neurons_out:
         robot = RobotYARP(mode=args.robot_mode)
     else:
         robot = None
@@ -162,23 +161,22 @@ if __name__ == "__main__":
     saveKey(uniqueId + "_output", output, output_dir)
 
     #Save the log
-    write_log(uniqueId, output_dir = output_dir)
+    write_log(uniqueId, output_dir=output_dir)
 
     #Print some statistic
     #TODO: verbosity
-    print ("\n\nSession: %s" % (uniqueId))
+    print "\n\nSession: %s" % (uniqueId)
     total_time = end_time - start_time
     print("-------------------\n\
 Total run time: %s\n\
 Realtime ratio: %sX\n\
 Steps: %s"
           % (
-              total_time
-              , output["ran_steps"]/(total_time*1000)
-              , output["ran_steps"]
-          )
-    )
-    print("-------------------\n")
+              total_time,
+              output["ran_steps"]/(total_time*1000),
+              output["ran_steps"]
+          ))
+    print "-------------------\n"
     #Show images
     if (
             (args.show_images or args.save_spikes or args.angle_images)
@@ -189,36 +187,38 @@ Steps: %s"
                 , args.save_spikes
                 , args.angle_images
             ])):
-        print("Processing images...")
+        print "Processing images..."
         from plugins.images import IO as ImageIO
         if args.show_spikes or args.save_spikes:
-            ImageIO.ImageFromSpikes(output["NeMo"][1]
-                        #used only if save true
-                        , file_path=output_dir + "/" + uniqueId + "_spikes.png"
-                        , show=all([args.show_spikes, args.show_images])
-                        , save=args.save_spikes
+            ImageIO.ImageFromSpikes(
+                output["NeMo"][1]
+                #used only if save true
+                , file_path=output_dir + "/" + uniqueId + "_spikes.png"
+                , show=all([args.show_spikes, args.show_images])
+                , save=args.save_spikes
             )
         if args.show_membrane:
             ImageIO.ImageFromMembranes(output["NeMo"][0])
         if args.angle_images:
             ImageIO.ImageFromAngles(
                 (output["YARP"]["read"],
-                output["YARP"]["wrote"])#FIXME: should be pyspike
-                , file_path=output_dir + "/" + uniqueId + "_angles.png")
+                 output["YARP"]["wrote"]), #FIXME: should be pyspike
+                file_path=output_dir + "/" + uniqueId + "_angles.png")
 
 #Analysis:
 if args.analyze_spikes_frequency: #TODO: write all conditions etc
     from plugins.analysis import spikes
     from plugins.importer import spikesDictToArray
     neuron_number = 0
-    print len (spikesDictToArray(output["NeMo"][1]))
+    print len(spikesDictToArray(output["NeMo"][1]))
     for i in spikesDictToArray(output["NeMo"][1]):
 #        print "ANALYSIS not working yet"
-        freq = spikes.getFreq(spikes.squareToBitSeq(spikes.neuronSpikesToSquare(i)))
+        #Fixme: changed function names, doesn't match anymore
+        freq = spikes.getFreq(spikes.neuronSpikesToSquare(i))
         if freq:
             print ("Neuron: %s, freqs: %s, %s"
                    %
                    (neuron_number, 1./freq[0]*1000, 1./freq[1]*1000))
         neuron_number += 1
-        
-print("All done, thanks!")
+
+print "All done, thanks!"
